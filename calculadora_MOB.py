7cbc20a9d2795d858023a756dfbc13a42c44825b
joaovbtb <joaovbtb@gmail.com>
2025-09-17 09:54:36 -0300

    comentários

--- calculadora_MOB.py
+++ calculadora_MOB.py
@@ -1,1 +1,19 @@
 #faremos nessa file a criação de uma calculadora implementando os conhecimentos que obtivemos no MOB
+
+#Operações necessárias:
+# adição, subtração, multiplicação, potenciação, exponencial (logarítmica), mod (resto), absoluto, trigonométricas
+
+#desafios citados:
+#Read.Me , Histórico de operações, tratamento de erros, testes unitários, simulação de conflitos (ultimo)
+
+
+
+
+
+
+
+#padronização dos commits [nome da sua branch] + :
+#_feat = add de novas funcionalidades
+#_fix = resolução de bugs
+#_docs = mudança da documentação
+#_refactor = melhoria da performance do código sem alterar o comportamento em si 
